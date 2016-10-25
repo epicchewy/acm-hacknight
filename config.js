25a157d6c073fc4e604415977053e800f4cfc90f
Luke Chui <chui.luke99@gmail.com>
2016-10-24 19:00:01 -0700

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+	consumer_key: "HoaLEJX66CUZJb3skPd39wgL2",
+	consumer_secret: "38kaEN96UmaRG30mIn3EvLgx0NaK2viyv2eJ4l7MpAj8oqJgjI",
+	access_token: "3262596740-JdwC4xCV7oXBvkPlxC4SSubpnSBI472Zfu4LZwb",
+	access_token_secret: "nZplw9SlfH3LZEYKcoBCUq6XEJexfRyh0HFUra3mSHhz2",
+	callback_url : "XXX"
+};
+
+module.exports = config;
